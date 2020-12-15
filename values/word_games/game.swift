43ae4b14ca7c1e8e371e38a7b07c34587f59e495
flyme2bluemoon <37197710+flyme2bluemoon@users.noreply.github.com>
2020-12-14 22:30:27 -0500

    Finished word games

---
+++ values/word_games/game.swift
@@ -0,0 +1,10 @@
+let steveJobsQuote = "Innovation distinguishes between a leader and a follower."
+
+let adjective = "Stupidity"
+let noun1 = "furry"
+let noun2 = "simp"
+
+let jeveSobsQuote = "\(adjective) distiguishes between a \(noun1) and a \(noun2)."
+
+print(steveJobsQuote)
+print(jeveSobsQuote)
