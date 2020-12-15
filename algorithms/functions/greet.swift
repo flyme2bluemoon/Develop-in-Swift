4a21da98157c8df6f6868f29a1b055b83815c141
flyme2bluemoon <37197710+flyme2bluemoon@users.noreply.github.com>
2020-12-15 18:18:20 -0500

    finished 2.2.1

---
+++ algorithms/functions/greet.swift
@@ -0,0 +1,16 @@
+func greet() {
+    print("Hello, world!")
+}
+
+func goodbye() {
+    print("Goodbye, developers!")
+}
+
+func main() {
+    greet()
+    goodbye()
+    // main()
+    // Uncommenting the above line creates an infinite recursive loop ;)
+}
+
+main()
